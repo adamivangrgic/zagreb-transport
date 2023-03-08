67108d2e6dd52e97b12b33dcea28f7f8322296c6
adamivangrgic <grgic.ivan.adam@gmail.com>
2023-03-08 17:49:27 +0100

    celery changes

--- zet_live/celery.py
+++ zet_live/celery.py
@@ -1,7 +1,5 @@
 import os
 from celery import Celery
-from celery.task import periodic_task
-from admin_utils.tasks import zet_sync
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zet_live.settings")
 app = Celery("zet_live")
