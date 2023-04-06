b78c4999e341a11451b37959f5c62e4a0f01a985
adamivangrgic <grgic.ivan.adam@gmail.com>
2023-04-06 12:47:45 +0000

    loading

--- search/urls.py
+++ search/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('search_suggestions/', views.search_suggestions, name='search_suggestions'),
     path('station/', views.station, name='station'),
     path('trip/', views.trip, name='trip'),
-    path('save_stop/', views.save_stop, name='save_stop'),
     path('route/', views.route, name='route'),
 
+    path('save_stop/', views.save_stop, name='save_stop'),
+    path('search_suggestions/', views.search_suggestions, name='search_suggestions'),
     path('location_search/', views.location_search, name='location_search'),
 ]
