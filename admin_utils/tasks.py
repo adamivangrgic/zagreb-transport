377d17db70faec9f092b836277457ce3638d182a
adamivangrgic <grgic.ivan.adam@gmail.com>
2023-05-05 20:16:44 +0000

    bug fix

--- admin_utils/tasks.py
+++ admin_utils/tasks.py
@@ -14,41 +14,38 @@ from zet_live.celery import app
 
 @app.task
 def update_static():
-    zet_feed = StaticFeed.objects.get(provider='zet')
-    hzpp_feed = StaticFeed.objects.get(provider='hzpp')
-    today = date.today()
-
-    zet_last_update = zet_feed.last_update
-    hzpp_last_update = hzpp_feed.last_update
-
-    zet_start_date = get_date_from_gtfs_static(zet.static_url, 3)
-
-    zet_latest_version_time = email.utils.parsedate_to_datetime(requests.request('HEAD', zet.static_url).headers['Last-Modified']).replace(tzinfo=None)
-    hzpp_latest_version_time = email.utils.parsedate_to_datetime(requests.request('HEAD', hzpp.static_url).headers['Last-Modified']).replace(tzinfo=None)
-
-    if zet_latest_version_time > zet_last_update and today >= zet_start_date:
+    try:
         update_zet()
-        zet_feed.last_update = zet_latest_version_time
-        zet_feed.save()
-
-    if hzpp_latest_version_time > hzpp_last_update:
+    except:
+        print('zet error')
+    
+    try:
         update_hzpp()
-        hzpp_feed.last_update = hzpp_latest_version_time
-        hzpp_feed.save()
+    except:
+        print('hzpp error')
 
 
 def update_zet(url=zet.static_url):
-    try:
+    zet_feed = StaticFeed.objects.get(provider='zet')
+    zet_last_update = zet_feed.last_update
+    zet_start_date = get_date_from_gtfs_static(zet.static_url, 3)
+    zet_latest_version_time = email.utils.parsedate_to_datetime(requests.request('HEAD', zet.static_url).headers['Last-Modified']).replace(tzinfo=None)
+
+    if zet_latest_version_time > zet_last_update and date.today() >= zet_start_date:
         zet.run_static_update(url)
-    except:
-        print('zet error')
+        zet_feed.last_update = zet_latest_version_time
+        zet_feed.save()
 
 
 def update_hzpp(url=hzpp.static_url):
-    try:
+    hzpp_feed = StaticFeed.objects.get(provider='hzpp')
+    hzpp_last_update = hzpp_feed.last_update
+    hzpp_latest_version_time = email.utils.parsedate_to_datetime(requests.request('HEAD', hzpp.static_url).headers['Last-Modified']).replace(tzinfo=None)
+
+    if hzpp_latest_version_time > hzpp_last_update:
         hzpp.run_static_update(url)
-    except:
-        print('hzpp error')
+        hzpp_feed.last_update = hzpp_latest_version_time
+        hzpp_feed.save()
 
 
 ### realtime
